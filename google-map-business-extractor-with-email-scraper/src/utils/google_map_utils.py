0c9090c1684e32c8dae2410ee6a41a2f8f120ec7
priscillaSmiths <priscillasmith7158@iejesusmirey.com>
2025-11-10 22:35:07 +0430

    feat: add google-map-business-extractor-with-email-scraper/src/utils/google_map_utils.py

---
+++ google-map-business-extractor-with-email-scraper/src/utils/google_map_utils.py
@@ -0,0 +1,6 @@
+thonimport requests
+
+def get_page_content(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    return response.text if response.status_code == 200 else None
